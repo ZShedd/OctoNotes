e030d8dbbce33336c55c3280b9032d4d121223d8
Clown Supreme <zachary.h.shedd@gmail.com>
2023-04-05 19:58:08 -0700

    added consts and function for readandappend in fsUtils.js

---
+++ helpers/ftUtils.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const util = require('util');
+
+const readFromFile = util.promisify(fs.readFile);
+
+const readAndAppend = (content, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const parsedData = JSON.parse(data);
+            parsedData.push(content);
+            fs.writeFile(file, JSON.stringify(parsedData), (err) =>
+                err ? console.error(err) : console.info(`\nData saved!`));
+        }
+    });
+}
+
+module.exports = { readFromFile }
