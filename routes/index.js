9c01cda7468fdf4d01549d3d8fa983bb0a3f6c5e
Clown Supreme <zachary.h.shedd@gmail.com>
2023-04-05 19:20:01 -0700

    Created consts and functions in server.js and index.js in routes

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+// Import the routes
+const notesRoute = require('./notes');
+
+app.use('/notes', notesRoute);
+
+module.exports = app;
